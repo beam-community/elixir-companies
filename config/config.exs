1c19e57917f57a419751e206bcfdf1200f31e866
George Mantzouranis <gemantzu@gmail.com>
2018-11-09 19:17:56 +0200

    bump versions

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,6 @@ config :logger, :console,
 
 # Use Jason for JSON parsing in Phoenix and Ecto
 config :phoenix, :json_library, Jason
-config :ecto, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
