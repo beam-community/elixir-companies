1b1ca6205774e56089a79d3522e70716ffdb7c2e
Sean Callan <seancallan@gmail.com>
2019-01-09 19:40:18 -0700

    Update directories and application name

--- lib/elixir_companies_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule CompaniesWeb.PageController do
-  use CompaniesWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Companies.MixProject do
 
   def project do
     [
-      app: :elixir_companies,
+      app: :companies,
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
