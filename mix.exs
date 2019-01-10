04bcc3b0393f0f3b5606d59364db5c26e62056ea
Sean Callan <seancallan@gmail.com>
2019-01-10 12:17:06 -0700

    Add TravisCI config

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Companies.MixProject do
     [
       app: :elixir_companies,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
