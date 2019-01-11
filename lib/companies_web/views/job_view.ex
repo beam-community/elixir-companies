80178dd35a1d43afb1ad71e71f6684e5feb73596
George Mantzouranis <gemantzu@gmail.com>
2019-01-11 23:06:38 +0200

    remake job form to accept company from select

--- lib/companies_web/views/job_view.ex
+++ lib/companies_web/views/job_view.ex
@@ -1,3 +1,9 @@
 defmodule CompaniesWeb.JobView do
   use CompaniesWeb, :view
+
+  def companies_for_select(companies) do
+    Enum.map(companies, fn company ->
+      {company.name, company.id}
+    end)
+  end
 end
