50768a3f9e0b5503bd178d7686443816a253af8b
Sean Callan <seancallan@gmail.com>
2019-02-02 13:38:05 -0700

    Heroku deployment changes

--- lib/companies/repo.ex
+++ lib/companies/repo.ex
@@ -2,4 +2,8 @@ defmodule Companies.Repo do
   use Ecto.Repo,
     otp_app: :companies,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+  end
 end
