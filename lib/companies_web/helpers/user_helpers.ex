958ef90245b52aec204ab112eb1c4ee59e3f0181
Sean Callan <seancallan@gmail.com>
2019-02-02 21:58:59 -0700

    Move signed_in?/1 to the helper module

--- lib/companies_web/helpers/user_helpers.ex
+++ lib/companies_web/helpers/user_helpers.ex
@@ -9,4 +9,12 @@ defmodule CompaniesWeb.UserHelpers do
   end
 
   def current_user(conn), do: conn.assigns[:current_user]
+
+  def signed_in?(conn) do
+    with %{id: _id} <- Map.get(conn.assigns, :current_user) do
+      true
+    else
+      _ -> false
+    end
+  end
 end
--- lib/companies_web/views/layout_view.ex
+++ lib/companies_web/views/layout_view.ex
@@ -1,11 +1,3 @@
 defmodule CompaniesWeb.LayoutView do
   use CompaniesWeb, :view
-
-  def signed_in?(conn) do
-    with %{id: _id} <- Map.get(conn.assigns, :current_user) do
-      true
-    else
-      _ -> false
-    end
-  end
 end
