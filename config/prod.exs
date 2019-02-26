2e2326a994d5947b6e3c561bc1a69e23518dc0dd
Sean Callan <seancallan@gmail.com>
2019-02-26 14:41:28 -0700

    Remove the beta subdomain

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :companies, CompaniesWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", host: "beta.elixir-companies.com"],
+  url: [scheme: "https", host: "elixir-companies.com"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
