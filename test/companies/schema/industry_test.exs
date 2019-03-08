d17851b76743f71102f065358c73ff74ede0fd11
WanjikuMac <edwardscaroline32@gmail.com>
2019-03-08 20:38:27 +0300

    minor changes to make the code readable

--- test/companies/schema/industry_test.exs
+++ test/companies/schema/industry_test.exs
@@ -11,16 +11,16 @@ defmodule Companies.Schema.IndustryTest do
   @valid_industry %{
     name: "Mining industry"
   }
-  @invalid_industry %{}
+
 
   describe "industry changeset " do
-    test "industry/1 validates with the correct data" do
+    test "industry/2 validates with the correct data" do
       changeset = Industry.changeset(%Industry{}, @valid_industry)
       assert changeset.valid?
     end
 
     test "errors out with invalid data" do
-      changeset = Industry.changeset(%Industry{}, @invalid_industry)
+      changeset = Industry.changeset(%Industry{}, %{})
       refute changeset.valid?
     end
   end
