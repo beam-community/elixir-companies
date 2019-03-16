dbaa3c42a6f0dc885c7aac9bf9729866972a27e8
Zacck Osiemo <coderv63@gmail.com>
2019-03-16 08:31:24 +0200

    beam out emails when they occur

--- lib/notify.ex
+++ lib/notify.ex
@@ -1,4 +1,16 @@
 defmodule Notify do
+  @moduledoc """
+  Context module that handles the sending of actual notifications 
+  """
+  alias Notify.{Email, Mailer}
+
+  @doc false
+  @spec perform(map()) :: Bamboo.Email.t() | term()
+  def perform(%{approved: true} = pending_change), do: pending_change |> Email.change_approved() |> Mailer.deliver_now()
+
+  def perform(%{approved: false} = pending_change),
+    do: pending_change |> Email.change_rejected() |> Mailer.deliver_now()
+
   def perform(message), do: apply(notifier(), :notify, [message])
 
   defp notifier, do: Application.get_env(:companies, :notifier)
