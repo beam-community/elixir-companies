9958725463cf37c7689e30df7198a6deb8d21c2a
Petr Stepchenko <nitrinodev@gmail.com>
2019-03-18 23:53:21 +0300

    Add locale selector

--- lib/companies_web/views/layout_view.ex
+++ lib/companies_web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule CompaniesWeb.LayoutView do
   use CompaniesWeb, :view
+
+  def current_location do
+    CompaniesWeb.Gettext
+    |> Gettext.get_locale()
+    |> String.capitalize()
+  end
+
+  def known_locales do
+    Gettext.known_locales(CompaniesWeb.Gettext)
+  end
 end
