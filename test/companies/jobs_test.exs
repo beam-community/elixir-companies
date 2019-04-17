bc5cc5762b21a8c803b8782d77c45936a8eab0ad
João Thallis <joaothallis.developer@gmail.com>
2019-04-17 09:44:01 -0300

    refactor: use @moduletag :capture_log
    Instead of capture_log function

--- test/companies/jobs_test.exs
+++ test/companies/jobs_test.exs
@@ -5,6 +5,8 @@ defmodule Companies.JobsTest do
 
   alias Companies.Jobs
 
+  @moduletag :capture_log
+
   setup do
     {:ok, %{user: insert(:user)}}
   end
@@ -18,12 +20,10 @@ defmodule Companies.JobsTest do
 
   describe "create/2" do
     test "creates a pending change for a new job when changes are valid", %{user: user} do
-      capture_log(fn ->
-        assert {:ok, %{action: "create", resource: "job"}} =
-                 :job
-                 |> params_for()
-                 |> Jobs.create(user)
-      end)
+      assert {:ok, %{action: "create", resource: "job"}} =
+               :job
+               |> params_for()
+               |> Jobs.create(user)
     end
 
     test "returns an error for invalid changes", %{user: user} do
@@ -33,23 +33,19 @@ defmodule Companies.JobsTest do
 
   describe "delete/2" do
     test "creates a pending change for deleting a job", %{user: user} do
-      capture_log(fn ->
-        assert {:ok, %{action: "delete", resource: "job"}} =
-                 :job
-                 |> insert()
-                 |> Jobs.delete(user)
-      end)
+      assert {:ok, %{action: "delete", resource: "job"}} =
+               :job
+               |> insert()
+               |> Jobs.delete(user)
     end
   end
 
   describe "update/3" do
     test "creates a pending change for job updates when changes are valid", %{user: user} do
-      capture_log(fn ->
-        assert {:ok, %{action: "update", resource: "job"}} =
-                 :job
-                 |> insert()
-                 |> Jobs.update(%{title: "updated"}, user)
-      end)
+      assert {:ok, %{action: "update", resource: "job"}} =
+               :job
+               |> insert()
+               |> Jobs.update(%{title: "updated"}, user)
     end
 
     test "returns an error for invalid changes", %{user: user} do
