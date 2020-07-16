b5dcc6ecbf85efb353fabf29680ef0b6cc508656
Brian Glusman <brian.glusman@pepsico.com>
2020-07-16 13:49:57 -0400

    Update Pheonix and LiveDashboardHistory (#622)
    Plus also use correct function in LDH config!

--- lib/companies_web/router.ex
+++ lib/companies_web/router.ex
@@ -41,7 +41,7 @@ defmodule CompaniesWeb.Router do
 
     live_dashboard "/",
       metrics: CompaniesWeb.Telemetry,
-      metrics_history: {LiveDashboardHistory, :data, [__MODULE__]}
+      metrics_history: {LiveDashboardHistory, :metrics_history, [__MODULE__]}
   end
 
   scope "/", CompaniesWeb do
