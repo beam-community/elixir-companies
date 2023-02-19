be137f908f4658ef730504dfcedfc00f74433a2b
Sean Callan <sean.callan@stord.com>
2023-02-19 09:49:02 -0500

    Update internal port

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ config :companies, CompaniesWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 8080],
   url: [scheme: "https", host: "elixir-companies.com"],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
