f1d3d2f5944cfe3d171817a3f9852ea11beef5eb
Sean Callan <seancallan@gmail.com>
2023-03-30 08:50:46 -0400

    Add Kelvin

---
+++ priv/companies/kelvin.exs
@@ -0,0 +1,15 @@
+%{
+  industry: "Clean Tech",
+  website: "https://www.kel.vin",
+  github: "https://github.com/orgs/Radiator-Labs/",
+  location: %{
+    city: "Brooklyn",
+    state: "NY",
+    country: "US"
+  },
+  name: "Kelvin (Radiator Labs)",
+  last_changed_on: ~D[2023-03-29],
+  description: """
+  Kelvin is a New York based start-up that is solving a 100-year-old problem: the lack of heating and cooling efficiency and comfort in buildings across the planet; causing enormous amounts of waste in both energy costs and carbon pollution.
+  """
+}
