fcf44d9c975ace098232b622575268b6e883a503
Herman verschooten <Herman@verschooten.net>
2023-09-07 18:07:04 +0200

    Add Octarion/Transport Nagels

---
+++ priv/companies/octarion.exs
@@ -0,0 +1,15 @@
+%{
+  industry: "Technology Consulting",
+  website: "https://www.octarion.eu/",
+  github: "https://github.com/Octarion",
+  location: %{
+    city: "Antwerp",
+    state: "",
+    country: "Belgium"
+  },
+  name: "Octarion",
+  last_changed_on: ~D[2023-09-07],
+  description: """
+  We use Elixir / Phoenix among other modern technologies, such as Nerves to support our own and customer's needs.
+  """
+}
