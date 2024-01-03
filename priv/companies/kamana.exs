b75d7ad1c27ff1047de731c6a2e4d4a51931f79b
Zahra Khan <55933076+za-h-ra@users.noreply.github.com>
2024-01-03 14:39:47 -0500

    Add KamanaHealth

---
+++ priv/companies/kamana.exs
@@ -0,0 +1,15 @@
+%{
+  industry: "Healthcare",
+  website: "https://www.kamana.com/",
+  github: "https://github.com/KamanaHealth",
+  location: %{
+    city: "Omaha",
+    state: "Nebraska",
+    country: "USA"
+  },
+  name: "Kamana",
+  last_changed_on: ~D[2023-03-01],
+  description: """
+  Kamana provides user-focused healthcare staffing software for healthcare staffing companies, nurses, and allied health professionals.
+  """
+}
