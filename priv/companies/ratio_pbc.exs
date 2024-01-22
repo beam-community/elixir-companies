27eeaf42ebc4aabb70aaaaba5fba339fa01a7fd2
Jesse Cooke <18191+jc00ke@users.noreply.github.com>
2024-01-22 15:02:51 -0800

    Add Ratio PBC (#695)

---
+++ priv/companies/ratio_pbc.exs
@@ -0,0 +1,15 @@
+%{
+  industry: "Technology",
+  website: "https://ratiopbc.com",
+  github: "https://github.com/RatioPBC",
+  location: %{
+    city: "Portland",
+    state: "Oregon",
+    country: "USA"
+  },
+  name: "Ratio PBC",
+  last_changed_on: ~D[2024-01-22],
+  description: """
+  We design, build, and operate secure, resilient, and human-centered software and data systems that empower health and human services organizations to improve the health and well-being of the communities they serve.
+  """
+}
