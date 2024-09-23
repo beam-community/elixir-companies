fefc5b8b141bcddd9c691dd256866e091c9a60d0
Sasha Gerrand <sgerrand@users.noreply.github.com>
2024-09-23 13:08:04 +0100

    Adds Duffel
    Adds a description for Duffel, an Elixir based API-first platform for managing travel bookings.

---
+++ priv/companies/duffel.exs
@@ -0,0 +1,15 @@
+%{
+  industry: "Leisure/Travel",
+  website: "https://duffel.com/",
+  github: "https://github.com/duffelhq/",
+  location: %{
+    city: "London",
+    state: "",
+    country: "United Kingdom"
+  },
+  name: "Duffel",
+  last_changed_on: ~D[2024-09-23],
+  description: """
+  Duffel is the complete toolkit to sell travel. Any business - from travel startups to financial enterprises - can build with Duffel. Shop flights and accommodation, purchase extras, manage bookings and more.
+  """
+}
