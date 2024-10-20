e36f160a63400d8e494de60ed9930516d8d15bc4
Mathias <m@mathiasnestler.de>
2024-10-20 10:59:26 +0200

    Add AccessOwl

---
+++ priv/companies/accessowl.exs
@@ -0,0 +1,16 @@
+%{
+  name: "AccessOwl",
+  website: "https://www.accessowl.com/",
+  github: "https://github.com/AccessOwl",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Technology",
+  location: %{
+    city: "Berlin",
+    state: "Berlin",
+    country: "Germany"
+  },
+  description: """
+  Automate Employee's access to any SaaS app. Manage SaaS access, apps and vendors in one place. Connect any app without upgrading it to an enterprise-subscription.
+  """,
+  last_changed_on: ~D[2024-10-20]
+}
