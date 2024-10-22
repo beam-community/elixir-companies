bd90700c1c9060d8fd5ef521ff514db92e96450b
Nathan Long <him@nathanmlong.com>
2024-10-22 10:41:36 -0400

    Add GridPoint (#707)

---
+++ priv/companies/gridpoint.exs
@@ -0,0 +1,19 @@
+# Company file for GridPoint
+# Created on: 2024-10-22
+
+%{
+  name: "GridPoint",
+  website: "https://www.gridpoint.com/",
+  github: "https://github.com/gridpoint-com/",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Energy",
+  location: %{
+    city: "",
+    state: "",
+    country: "United States"
+  },
+  description: """
+  Hardware and SaaS company enabling energy efficiency for businesses. Accelerating the transition to a sustainable energy future. Remote development team using Phoenix and Nerves. Creators of https://hex.pm/packages/plox
+  """,
+  last_changed_on: ~D[2024-10-22]
+}
