08656194829e14b41645bd1061d684e8df9aedaf
wlad <wladimirgramacho@gmail.com>
2024-10-30 15:52:43 -0300

    Create uplearn.exs

---
+++ priv/companies/uplearn.exs
@@ -0,0 +1,19 @@
+# Company file for Up Learn
+# Created on: 2024-10-30
+
+%{
+  name: "Up Learn",
+  website: "https://uplearn.co.uk/",
+  github: "https://gitlab.com/up-learn-uk",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Education",
+  location: %{
+    city: "London",
+    state: "London",
+    country: "United Kingdom"
+  },
+  description: """
+  Up Learn is an UK EdTech that provides everything you need to get top grades at UK exams (A Level and GCSE) in your subject, as efficiently as possible. No need for textbooks or tutors.
+  """,
+  last_changed_on: ~D[2024-10-30]
+}
