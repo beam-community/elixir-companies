ec33dd453cb90f69faa446568a44c80e586ed06c
wlad <wladimirgramacho@gmail.com>
2024-11-01 13:33:03 -0300

    Add Up Learn (#718)
    * Create uplearn.exs
    
    * Update uplearn.exs
    
    * Update priv/companies/uplearn.exs
    
    ---------
    
    Co-authored-by: Sean Callan <doomspork@users.noreply.github.com>
    Co-authored-by: Jim Segal <hello@jimsegal.com>

---
+++ priv/companies/uplearn.exs
@@ -0,0 +1,20 @@
+# Company file for Up Learn
+# Created on: 2024-10-30
+
+%{
+  name: "Up Learn",
+  website: "https://uplearn.co.uk/",
+  github: "https://gitlab.com/up-learn-uk",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Information Technology",
+  location: %{
+    city: "London",
+    state: "London",
+    country: "United Kingdom"
+  },
+  description: """
+  Up Learn is an UK EdTech that provides everything you need to get top grades at UK exams (A Level and GCSE) in your subject, as efficiently as possible. No need for textbooks or tutors.
+  We use Elixir and React to build our main application, serving multiple courses to tens of thousands of students daily.
+  """,
+  last_changed_on: ~D[2024-10-30]
+}
