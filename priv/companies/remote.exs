eee6e7c8a4c68a7688e0c3088e7eafd7ba4fcfbc
Vincent <vincent.trubesset@gmail.com>
2024-11-14 17:36:29 +0100

    add: remote.com (#727)

---
+++ priv/companies/remote.exs
@@ -0,0 +1,20 @@
+# Company file for remote
+# Created on: 2024-11-14
+
+%{
+  name: "Remote",
+  website: "https://remote.com/",
+  github: "https://github.com/remoteoss",
+  industry: "Information Technology",
+  location: %{
+    city: "San Francisco",
+    state: "California",
+    country: "United States of America"
+  },
+  description: """
+  Remote is a Global HR Platform that helps companies hire, manage, and pay their entire team — and more effectively compete in the modern global economy through our comprehensive set of core solutions including, HRIS, payroll, international employment, contractor management, and more.
+
+  Their backend is built with Elixir and Phoenix.
+  """,
+  last_changed_on: ~D[2024-11-14]
+}
