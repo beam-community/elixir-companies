47b878a647fc69b83212b3261ace9b9171c480f6
David Alencar <davidalencarignacio@gmail.com>
2024-11-15 12:55:04 -0300

    add trio

---
+++ priv/companies/trio.exs
@@ -0,0 +1,17 @@
+  %{
+    name: "Trio",
+    website: "https://www.triohq.com/en",
+    github: "https://github.com/trioHQ",
+    # reference lib/companies/industries.ex for a list of recommended industries to use here
+    industry: "Financials",
+    location: %{
+      city: "Curitiba",
+      state: "Paraná",
+      country: "Brazil"
+    },
+    description: """
+    Pix infrastructure to evolve in real-time payments.
+    """,
+    last_changed_on: ~D[2024-11-15]
+  }
+
