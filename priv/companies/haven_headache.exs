2ba19c4400829dd3a162181e378d84269e737a13
amattn <amattn@users.noreply.github.com>
2024-11-20 08:06:32 -0800

    add: Haven Headache & Migraine Center
    https://havenheadache.com

---
+++ priv/companies/haven_headache.exs
@@ -0,0 +1,21 @@
+# Company file for Haven Headache
+# Created on: 2024-11-20
+
+%{
+  name: "Haven Headache & Migraine Center",
+  website: "https://havenheadache.com/",
+  github: "https://github.com/havenhm",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Health Care",
+  location: %{
+    city: "San Francisco",
+    state: "CA",
+    country: "United States"
+  },
+  description: """
+  Haven is a tech-enabled virtual telehealth solution for the millions of people who suffer from migraine. We offer the latest clinically proven care pathways to help patients get their condition under control in less time.
+
+  Our backend patient management stack is built with Elixir, Phoenix and LiveView.
+  """,
+  last_changed_on: ~D[2024-11-20]
+}
