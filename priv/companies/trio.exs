2344a526a8dfb14d48baa36ff2aaa845d439c42f
Sean Callan <doomspork@users.noreply.github.com>
2024-11-26 16:05:45 -0500

    Update priv/companies/trio.exs
    Co-authored-by: Jim Segal <hello@jimsegal.com>

--- priv/companies/trio.exs
+++ priv/companies/trio.exs
@@ -1,17 +1,16 @@
-  %{
-    name: "Trio",
-    website: "https://www.triohq.com/en",
-    github: "https://github.com/trioHQ",
-    # reference lib/companies/industries.ex for a list of recommended industries to use here
-    industry: "Financials",
-    location: %{
-      city: "Curitiba",
-      state: "Paraná",
-      country: "Brazil"
-    },
-    description: """
-    Trio is a financial institution specializing in instant payments in Brazil. All our products, from the banking core to the payment checkout, were developed using Elixir, Phoenix, and LiveView.
-    """,
-    last_changed_on: ~D[2024-11-15]
-  }
-
+%{
+  name: "Trio",
+  website: "https://www.triohq.com/en",
+  github: "https://github.com/trioHQ",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Financials",
+  location: %{
+    city: "Curitiba",
+    state: "Paraná",
+    country: "Brazil"
+  },
+  description: """
+  Trio is a financial institution specializing in instant payments in Brazil. All our products, from the banking core to the payment checkout, were developed using Elixir, Phoenix, and LiveView.
+  """,
+  last_changed_on: ~D[2024-11-15]
+}
