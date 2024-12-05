b7434b71c1e96086acd4063f669edab9c064ef67
Patrik Stenmark <patrik@stenmark.io>
2024-12-05 10:35:22 +0100

    Add All Aboard

---
+++ priv/companies/all_aboard.exs
@@ -0,0 +1,22 @@
+# Company file for All Aboard
+# Created on: 2024-12-05
+
+%{
+  name: "All Aboard",
+  website: "https://allaboard.eu/",
+  github: "https://github.com/allaboard-dev",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Technology",
+  location: %{
+    city: "Stockholm",
+    state: "Stockholm",
+    country: "Sweden"
+  },
+  description: """
+  Easy-to-use booking API and platform for European train tickets.
+
+  Using Elixir to power backend with integrations to train carriers, order
+  and payment handling and more.
+  """,
+  last_changed_on: ~D[2024-12-05]
+}
