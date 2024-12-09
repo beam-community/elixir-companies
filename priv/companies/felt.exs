a6516bb4a540f79fa762e2e04c4df175fb2cb408
Chad Lawlis <1238636+chadlawlis@users.noreply.github.com>
2024-12-09 09:12:55 -0500

    Add Felt (#734)

---
+++ priv/companies/felt.exs
@@ -0,0 +1,17 @@
+%{
+  name: "Felt",
+  website: "https://felt.com",
+  github: "https://github.com/felt",
+  industry: "Information Technology",
+  location: %{
+    city: "Oakland",
+    state: "CA",
+    country: "USA"
+  },
+  description: """
+  A cloud-native geographic information system (GIS) platform for building maps, apps, and dashboards.
+
+  Powered by Elixir and Phoenix.
+  """,
+  last_changed_on: ~D[2024-12-09]
+}
