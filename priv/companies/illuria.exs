b2f6dbaec178f22152b47d9d09be89c7bfa7a2dc
Antranig Vartanian <antranigv@freebsd.am>
2024-12-28 05:01:22 +0400

    Add illuria, Inc.

---
+++ priv/companies/illuria.exs
@@ -0,0 +1,18 @@
+# Company file for illuria
+# Created on: 2024-12-28
+
+%{
+  name: "illuria, Inc.",
+  website: "https://illuria.com",
+  github: "https://github.com/illuria",
+  industry: "Information Technology",
+  location: %{
+    city: "",
+    state: "",
+    country: ""
+  },
+  description: """
+  illuria, Inc. provides a wide range of IT services for companies of all sizes, from small businesses to large enterprises. Makers of ProfilerX (Honeypots++) and Jailer (FreeBSD Jails, Networking and Storage orchestration).
+  """,
+  last_changed_on: ~D[2024-12-28]
+}
