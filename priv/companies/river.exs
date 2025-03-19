68ca8391d3239dd8af3d8372557c2007d28f8c9b
KaFai Choi <kafai.choi@river.com>
2025-03-19 18:01:36 +0700

    add river

---
+++ priv/companies/river.exs
@@ -0,0 +1,19 @@
+# Company file for River
+# Created on: 2025-03-19
+
+%{
+  name: "River",
+  website: "https://river.com",
+  github: "https://github.com/RiverFinancial",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Financial Technology",
+  location: %{
+    city: "Columbus",
+    state: "Ohio",
+    country: "United States"
+  },
+  description: """
+  River is a U.S.-based Bitcoin technology and financial services company offering bitcoin brokerage and full-reserve custody services. Our systems architecture leverages Elixir technology to ensure secure, scalable, and reliable financial operations.
+  """,
+  last_changed_on: ~D[2025-03-19]
+}
