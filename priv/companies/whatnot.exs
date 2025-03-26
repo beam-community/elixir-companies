2e1b0ed34863e120896d163c933e535a04166d0f
Paweł Pikuła <ppikula@gmail.com>
2025-03-26 17:19:46 +0100

    Add Whatnot

---
+++ priv/companies/whatnot.exs
@@ -0,0 +1,19 @@
+# Company file for whatnot
+# Created on: 2025-03-26
+
+%{
+  name: "Whatnot",
+  website: "https://whatnot.com/",
+  github: "https://github.com/whatnot-Inc",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Marketplace",
+  location: %{
+    city: "",
+    state: "",
+    country: "Remote (USA/EU)"
+  },
+  description: """
+  Whatnot is the largest livestream shopping platform in North America and Europe to buy, sell, and discover the things you love. Elixir is powering our Live Stream auctions and chat.
+  """,
+  last_changed_on: ~D[2025-03-26]
+}
