72d1ef3829af0f05c110206344087b3122509277
hibernatus-hacker <hibernatus@tutamail.com>
2025-04-05 14:30:56 -0400

    add hedgehog AI

---
+++ priv/companies/hedgehog_ai.exs
@@ -0,0 +1,19 @@
+# Company file for hedgehog_ai
+# Created on: 2025-04-05
+
+%{
+  name: "hedgehog_ai",
+  website: "https://example.com/",
+  github: "https://github.com/example/hedgehog_ai",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Information Technology",
+  location: %{
+    city: "City",
+    state: "State",
+    country: "Country"
+  },
+  description: """
+  Description of hedgehog_ai goes here.
+  """,
+  last_changed_on: ~D[2025-04-05]
+}
