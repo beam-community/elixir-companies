faec4775e27c6c29f3f4ffd26f604423ba6987e6
hibernatus-hacker <hibernatus@tutamail.com>
2025-04-05 14:35:54 -0400

    add hedgehog AI

--- priv/companies/hedgehog_ai.exs
+++ priv/companies/hedgehog_ai.exs
@@ -3,17 +3,17 @@
 
 %{
   name: "hedgehog_ai",
-  website: "https://example.com/",
-  github: "https://github.com/example/hedgehog_ai",
+  website: "https://hedgehog.chat",
+  github: "https://github.com/hibernatus_hacker",
   # reference lib/companies/industries.ex for a list of recommended industries to use here
   industry: "Information Technology",
   location: %{
-    city: "City",
-    state: "State",
-    country: "Country"
+    city: "London",
+    state: "London",
+    country: "United Kingdom"
   },
   description: """
-  Description of hedgehog_ai goes here.
+  Hedgehog AI is a bootstrapped Elixir AI startup aiming for a 100% self hosted ML distributed elixir stack. Our first product is a 90s themes AI search tool that works entirely without javascript making it suitable for command line browers and very old computers.
   """,
   last_changed_on: ~D[2025-04-05]
 }
