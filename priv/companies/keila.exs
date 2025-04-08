b8aaade0bcb4cd5bce7c173c648925db77a3cfd7
Philipp <git@pentacent.com>
2025-04-08 22:48:48 +0200

    Update keila.exs

--- priv/companies/keila.exs
+++ priv/companies/keila.exs
@@ -9,5 +9,6 @@
   last_changed_on: ~D[2025-04-08],
   description: """
   Keila is an Open Source email newsletter tool and an alternative to MailChimp. It’s available as a managed service (Keila Cloud) but can also be self-hosted.
+  Keila is built using the Phoenix framework and uses various common Elixir libraries, including Ecto, Oban, and Earmark.
   """
 }
