9d5231ea2ddb53d946e09a3568ebdc3a6252aa57
Adam Kirk <atomkirk@gmail.com>
2025-05-17 11:37:13 -0600

    Create jump.exs

---
+++ priv/companies/jump.exs
@@ -0,0 +1,13 @@
+%{
+  industry: "Financial Services",
+  website: "https://www.jumpapp.com/",
+  github: "https://github.com/Jump-App",
+  location: %{
+    country: "US"
+  },
+  name: "Jump",
+  last_changed_on: ~D[2025-05-17],
+  description: """
+  Jump is AI for Financial Advisors. We're all Elixir. Come work with us! careers.jumpapp.com
+  """
+}
