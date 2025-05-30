c3b6953f45387a565ebedfc4a703dff2a7acba34
Dave Lucia <davelucianyc@gmail.com>
2025-05-30 10:59:46 -0400

    feat: add TV Labs (#766)
    * add TV Labs
    
    * mention elixir

---
+++ priv/companies/tv_labs.exs
@@ -0,0 +1,22 @@
+# Company file for tv_labs
+# Created on: 2025-05-30
+
+%{
+  name: "TV Labs",
+  website: "https://tvlabs.ai",
+  github: "https://github.com/tv-labs",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Information Technology",
+  location: %{
+    city: "New York",
+    state: "New York",
+    country: "USA"
+  },
+  description: """
+  TV Labs is a Smart TV testing platform, providing virtualized access and testing infrastructure
+  to physical televisions and streaming media devices. Elixir is at the core of TV Labs solution,
+  handling the orchestration and communication with devices, as well as serving its APIs and web service.
+  TV Labs also makes use of Luerl, an implementation of Lua on the BEAM.
+  """,
+  last_changed_on: ~D[2025-05-30]
+}
