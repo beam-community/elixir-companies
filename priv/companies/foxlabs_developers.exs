cea0d3022e46ebef244730c81261e16c66fa4b11
Jorge Baez <jorge@foxlabsdevelopers.com>
2025-05-30 00:52:57 -0500

    Feat: Add foxlabs developers company file

---
+++ priv/companies/foxlabs_developers.exs
@@ -0,0 +1,17 @@
+# Company file for Foxlabs Developers
+%{
+# reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Information Technology",
+  website: "https://foxlabsdevelopers.com",
+  github: "https://github.com/foxlabsdevelopers",
+  location: %{
+    city: "Playa del Carmen",
+    state: "Quintana Roo",
+    country: "Mexico"
+  },
+  name: "Foxlabs Developers",
+  last_changed_on: ~D[2025-05-29],
+  description: """
+  Custom software, Elixir systems, product strategy, & code audits. Based in Mexico, working globally. Experts in scalable platforms. Builders of our own CMS.
+  """
+}
