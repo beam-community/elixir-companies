2053e4fd0ee9f1dbcc1ed1189e2ef53c53155e67
Dave Lucia <davelucianyc@gmail.com>
2025-05-30 10:55:06 -0400

    add TV Labs

---
+++ priv/companies/tv_labs.exs
@@ -0,0 +1,20 @@
+# Company file for tv_labs
+# Created on: 2025-05-30
+
+%{
+  name: "TV Labs",
+  website: "https://tvlabs.ai",
+  github: "https://github.com/tv-labs",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Information Technology",
+  location: %{
+    city: "New York",
+    state: "New York",
+    country: "USA"
+  },
+  description: """
+  TV Labs is a Smart TV testing platform, providing virtualized access and testing infrastructure
+  to physical televisions and streaming media devices.
+  """,
+  last_changed_on: ~D[2025-05-30]
+}
