eb83586730613626bb7d83a97ddc23859c14da99
Mackenzie <macoafi@gmail.com>
2025-07-03 13:11:40 -0400

    add Level All (#774)

---
+++ priv/companies/level_all.exs
@@ -0,0 +1,16 @@
+%{
+  industry: "Education",
+  website: "https://www.levelall.com/",
+  github: "https://github.com/Level-All",
+  location: %{
+    city: "New York City",
+    state: "New York",
+    country: "USA"
+  },
+  name: "Level All",
+  last_changed_on: ~D[2025-07-03],
+  description: """
+  Improving outcomes for students and leveling the playing field for all.
+  Level All is built using the Phoenix framework with LiveView and Ash, along with common Elixir libraries, such as Ecto and Oban.
+  """
+}
