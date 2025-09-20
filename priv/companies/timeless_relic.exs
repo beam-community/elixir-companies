79956295f05bdd5b865f4066769186a0c3823917
Miguel Carneiro <mig4ng@gmail.com>
2025-09-20 03:00:30 +0100

    Add Timeless Relic to the company directory

---
+++ priv/companies/timeless_relic.exs
@@ -0,0 +1,17 @@
+# Company file for Timeless Relic
+# Created on: 2025-09-20
+
+%{
+  name: "Timeless Relic",
+  website: "https://timelessrelic.com/",
+  github: "https://github.com/TimelessRelic",
+  industry: "Information Technology",
+  location: %{
+    country: "Remote"
+  },
+  description: """
+  Polyglot development (but highly prefer Elixir/Erlang) and consulting.
+  Vast infrastructure knowledge (DevOps mindset), security, system administration, CI/CD, automation, and AI.
+  """,
+  last_changed_on: ~D[2025-09-20]
+}
