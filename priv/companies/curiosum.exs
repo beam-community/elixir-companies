1db6c6b35db1cdc29d587fb82c333d34fc84cc64
Szymon Soppa <szymon@curiosum.com>
2025-12-19 19:57:04 +0100

    Adds Curiosum company (#805)

---
+++ priv/companies/curiosum.exs
@@ -0,0 +1,18 @@
+# Company file for Curiosum
+# Created on: 2025-12-19
+
+%{
+  name: "Curiosum",
+  website: "https://curiosum.com/",
+  github: "https://github.com/curiosum-dev",
+  industry: "Information Technology",
+  location: %{
+    city: "Poznan",
+    state: "Wielkopolskie",
+    country: "Poland"
+  },
+  description: """
+  Curiosum is an Elixir-specialized software development agency with 50+ production-ready projects delivered using Elixir and Phoenix for clients worldwide. We build products from scratch or support teams with expert Elixir developers through team extension.
+  """,
+  last_changed_on: ~D[2025-12-19]
+}
