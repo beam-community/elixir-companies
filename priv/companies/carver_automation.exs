33ce0afc51abb421cb13289d6d8645c230e3f2ee
Michael Freeman <mfreeman@carverauto.dev>
2025-12-28 15:22:22 -0600

    feat: adding carver auto (#806)
    * adding carver auto
    
    * updating desc

---
+++ priv/companies/carver_automation.exs
@@ -0,0 +1,19 @@
+# Company file for Carver Automation
+# Created on: 2025-12-28
+
+%{
+  name: "Carver Automation",
+  website: "https://serviceradar.cloud/",
+  github: "https://github.com/carverauto/serviceradar",
+  # reference lib/companies/industries.ex for a list of recommended industries to use here
+  industry: "Information Technology",
+  location: %{
+    city: "Carver",
+    state: "MN",
+    country: "US"
+  },
+  description: """
+  Makers of Open Source Network Management and Observability software, powered by elixir and the BEAM vm.
+  """,
+  last_changed_on: ~D[2025-12-28]
+}
