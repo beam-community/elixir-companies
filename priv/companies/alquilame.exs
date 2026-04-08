bc0c5fcef2820284786c6118c498edaa1de544a0
Oscar Olivera <oscar@alquilame.io>
2026-04-08 20:30:33 +0200

    Add Alquilame.io (#826)

---
+++ priv/companies/alquilame.exs
@@ -0,0 +1,15 @@
+%{
+  name: "Alquilame.io",
+  website: "https://alquilame.io",
+  github: "https://github.com/alquilame-io",
+  industry: "Information Technology",
+  location: %{
+    city: "Mexico City",
+    state: "CDMX",
+    country: "Mexico"
+  },
+  description: """
+  Multi-tenant SaaS platform built with Phoenix LiveView for rental businesses. Helps rental companies manage their inventory, bookings, and customer-facing storefronts with a simple white-label solution.
+  """,
+  last_changed_on: ~D[2026-04-08]
+}
